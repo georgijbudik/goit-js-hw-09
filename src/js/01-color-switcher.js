a2528aad6fcc66a91ad4af5c3174f1b67bf56e64
georgijbudik <georgijbudik@gmail.com>
2023-07-17 16:00:16 +0200

    finish hw

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+
+let intervalId = null;
+stopBtnEl.disabled = true;
+
+const onStartClick = () => {
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+  timerId = setInterval(bodyColorChange, 1000);
+};
+
+const onStopClick = () => {
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+  clearInterval(timerId);
+};
+
+const bodyColorChange = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+startBtnEl.addEventListener('click', onStartClick);
+stopBtnEl.addEventListener('click', onStopClick);
