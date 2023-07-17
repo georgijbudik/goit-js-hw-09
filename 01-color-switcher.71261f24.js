50e9ed5bb103e9198514b24c38a80b4f8f8bbc72
georgijbudik <125678048+georgijbudik@users.noreply.github.com>
2023-07-17 14:37:31 +0000

    Deploying to gh-pages from @ georgijbudik/goit-js-hw-09@c4bc9fa6659a7ccf85501ee75701bafcc7252995 🚀

---
+++ 01-color-switcher.71261f24.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=null;e.disabled=!0;var d=function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))};t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval(d,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.71261f24.js.map
